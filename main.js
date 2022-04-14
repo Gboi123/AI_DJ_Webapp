6473a1b4a0e07cec94b1eecc31687b3cfcd91b10
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2022-04-14 19:56:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+Michael_Jackson_song="";
+old_town_road_song="";
+
+function preload(){
+    Michael_Jackson_song = loadSound("Michael_Jackson.mp3");
+    old_town_road_song = loadSound("old_town_road.mp3");
+}
 function setup(){
     canvas = createCanvas(600,450);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+
+
+function draw(){
+    image(video,0,0,600,530);
 }
