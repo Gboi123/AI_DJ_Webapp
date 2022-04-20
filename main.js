9368405cbcc9e5aca58d8e674531336317078880
Gboi123 <85195842+Gboi123@users.noreply.github.com>
2022-04-20 19:30:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,67 @@
 Michael_Jackson_song="";
 old_town_road_song="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+scoreleftWrist = 0;
+song_name = "";
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
 
 function preload(){
     Michael_Jackson_song = loadSound("Michael_Jackson.mp3");
     old_town_road_song = loadSound("old_town_road.mp3");
 }
-function setup(){
-    canvas = createCanvas(600,450);
-    canvas.center();
-
-    video = createCapture(VIDEO);
-    video.hide();
-}
-
-
 
 function draw(){
     image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_name = old_town_road_song.isPlaying();
+    console.log(song_name);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+       Michael_Jackson_song.stop();
+        if(song_name == false){
+            old_town_road_song.play();
+        }
+        else{
+            console.log("Song Name: Peter Pan Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Peter Pan Song";
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
 }
